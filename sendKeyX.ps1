6cebce8928845f5006aead18b82bebef0f42bb83
Peach-Iced-Tea <134111163+Peach-Iced-Tea@users.noreply.github.com>
2024-11-14 19:54:47 +0100

    Update sendKeyX.ps1

--- sendKeyX.ps1
+++ sendKeyX.ps1
@@ -1,3 +1,4 @@
-Add-Type -AssemblyName System.Windows.Forms
-
-[System.Windows.Forms.SendKeys]::SendWait("^{c}")
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('WebBrowserPassView')
+Sleep 2
+$wshell.SendKeys("^{a}")
